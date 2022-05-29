eab3b5034a96d5171bff7b8a9af4e565f2846cf3
adriavieira314 <adriavieira731@gmail.com>
2022-05-29 19:07:25 -0400

    tela para preenchimento do cartao de credito

---
+++ lib/src/features/credit_card/pages/credit_card_screen.text.dart
@@ -0,0 +1,16 @@
+class CreditCardScreenText {
+  static const String title = 'Cartão de Crédito';
+  static const String parcelas = 'Número de Parcelas';
+  static const String cartao = 'Número do Cartão';
+  static const String validade = 'Validade';
+  static const String cvv = 'CVV';
+  static const String nomeImpresso = 'Nome Impresso';
+  static const String cpfCnpj = 'CPF/CNPJ';
+
+  static List<String> listOfParcels = [
+    '1x de R\$ 461,17',
+    '2x de R\$ 238,55, total de R\$ 477,09',
+    '3x de R\$ 162,25, total de R\$ 486,76',
+    '4x de R\$ 122,91, total de R\$ 491,63'
+  ];
+}
